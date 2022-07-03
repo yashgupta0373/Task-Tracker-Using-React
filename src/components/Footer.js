c8806f698dcc27f64e4abd96216b7ce035bc24b3
Yash Gupta <yashgupta0373@gmail.com>
2022-07-03 14:06:50 +0530

    First upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import {Link} from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <footer>
+            <Link to="/yashgupta">Made by Yash Gupta</Link>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/YashGupta.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+const YashGupta = () => {
+    return (
+        <div>
+            <h4>Made by Yash Gupta</h4>
+            <Link to="/">Back</Link>
+        </div>
+    )
+}
+
+export default YashGupta
